2fe7507451c655e2701d61cc261fca208490d2b3
Forooz99 <forooz.iraji@gmail.com>
2024-02-04 07:57:11 +0330

    edit journal url

--- journal/urls.py
+++ journal/urls.py
@@ -3,7 +3,7 @@ from journal.views import *
 
 urlpatterns = [
     path('volumes', VolumesAPI.as_view()),
-    path('volumes/<slug:pk>', VolumeByIdAPI.as_view()),
+    path('volumes/<str:pk>', VolumeByIdAPI.as_view()),
     path('<slug:pk>', JournalByIdAPI.as_view()),
     path('', JournalsAPI.as_view()),
 ]
